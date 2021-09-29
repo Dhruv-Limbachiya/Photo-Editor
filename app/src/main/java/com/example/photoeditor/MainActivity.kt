fe1c271c34d4b6cf9dd78926469c5827a3816695
Dhruv-Limbachiya <dhruv.mail4work@gmail.com>
2021-09-29 18:02:30 +0530

    change status bar color

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.photoeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
